8f7eab3bc101f97045cb29a8344f2caf2032562c
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-05-07 17:44:26 +0400

    moved objects to new folder and renamed

--- Assignment2/objects/objects.py
+++ Assignment2/objects/objects.py
@@ -3,8 +3,8 @@ import pyrosim
 sim = pyrosim.Simulator( play_paused=True, eval_time=1000)
 
 #create new cylinder and capture its ID in a variable
-whiteObject = sim.send_cylinder(x=0, y=0, z=0.6 ,length=1.0, radius=0.1)
-redObject = sim.send_cylinder(x=-0.2, y=0, z=0.6, r=1, g=0, b=0, r1=0,r2=1,r3=0)
+whiteObject = sim.send_cylinder(x=0, y=0, z=0.6,length=1.0, radius=0.1)
+redObject = sim.send_cylinder(x=0, y=0.5, z=1.1, length=1.0, r=1, g=0, b=0, r1=0,r2=1,r3=0)
 
 sim.start()
 sim.wait_to_finish()
