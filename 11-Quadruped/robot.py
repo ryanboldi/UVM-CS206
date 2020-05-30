b08256b8bf462311415af4585a834b26488a8f0f
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-05-30 22:31:54 +0400

    finished 4th video

--- 11-Quadruped/robot.py
+++ 11-Quadruped/robot.py
@@ -1,5 +1,6 @@
 import constants as c
 import math
+import random
 
 
 class ROBOT:
@@ -8,7 +9,15 @@ class ROBOT:
         self.send_joints(sim)
         self.send_sensors(sim)
         self.send_neurons(sim)
-        #self.send_synapses(sim, wts)
+        self.send_synapses(sim, wts)
+
+
+        # delete these so they're not copied with copy.deepcopy()
+        del self.O
+        del self.J
+        del self.S
+        del self.SN
+        del self.MN
         
 
     def send_objects(self, sim):
@@ -25,6 +34,16 @@ class ROBOT:
         self.O7 = sim.send_cylinder(x=0, y=-1.5*c.L, z = (c.L/2) + c.R, length=c.L, radius=c.R, r1=0, r2=0, r3=1, r=0, g=0, b=1)
         self.O8 = sim.send_cylinder(x=-1.5*c.L, y=0, z = (c.L/2) + c.R, length=c.L, radius=c.R, r1=0, r2=0, r3=1, r=1, g=0, b=1)
 
+        self.O = {} #dict of all objects    
+        self.O[0] = self.O0
+        self.O[1] = self.O1
+        self.O[2] = self.O2
+        self.O[3] = self.O3
+        self.O[4] = self.O4
+        self.O[5] = self.O5
+        self.O[6] = self.O6
+        self.O[7] = self.O7
+        self.O[8] = self.O8
 
 
     def send_joints(self,sim):
@@ -38,6 +57,15 @@ class ROBOT:
         self.J6 = sim.send_hinge_joint(first_body_id=self.O0, second_body_id=self.O4,x=-c.L/2, y=0, z=c.L + c.R,n1=0, n2=-1, n3=0, hi=math.pi /2, lo=-math.pi/2)
         self.J6 = sim.send_hinge_joint(first_body_id=self.O4, second_body_id=self.O8,x=-1.5*c.L, y=0, z=c.L + c.R,n1=0, n2=-1, n3=0, hi=math.pi /2, lo=-math.pi/2)
 
+        self.J = {}
+        self.J[0] = self.J0
+        self.J[1] = self.J1
+        self.J[2] = self.J2
+        self.J[3] = self.J3
+        self.J[4] = self.J4
+        self.J[5] = self.J5
+        self.J[6] = self.J6
+
 
     def send_sensors(self, sim):
         self.T0 = sim.send_touch_sensor(body_id = self.O5)
@@ -45,10 +73,30 @@ class ROBOT:
         self.T2 = sim.send_touch_sensor(body_id = self.O7)
         self.T3 = sim.send_touch_sensor(body_id = self.O8)
 
+        self.S = {}
+        self.S[0] = self.T0
+        self.S[1] = self.T1
+        self.S[2] = self.T2
+        self.S[3] = self.T3
+
+
         #Only used for fitness computation   
-        #self.P4 = sim.send_position_sensor( body_id=self.redObject)
+        self.P4 = sim.send_position_sensor( body_id=self.O0)
 
     def send_neurons(self, sim):
+        self.SN = {}
+
+        for s in self.S:
+            self.SN[s] = sim.send_sensor_neuron(sensor_id=self.S[s])
+        
+        self.MN = {}
+
+        for j in self.J:
+            self.MN[j] = sim.send_motor_neuron(joint_id=self.J[j])
+
+
+
+
          #creates sensor neurons, each one connected to one cylinder
         #self.SN0 = sim.send_sensor_neuron( sensor_id=self.T0 )
         #self.SN1 = sim.send_sensor_neuron( sensor_id=self.T1 )
@@ -61,15 +109,6 @@ class ROBOT:
 
 
     def send_synapses(self, sim, wts):
-        sensorNeurons = {}
-        sensorNeurons[0] = self.SN0
-        sensorNeurons[1] = self.SN1
-        sensorNeurons[2] = self.SN2
-        sensorNeurons[3] = self.SN3
-
-        motorNeurons = {}
-        motorNeurons[0] = self.MN2
-
-        for s in sensorNeurons:
-            for m in motorNeurons:  
-                sim.send_synapse(source_neuron_id=sensorNeurons[s],target_neuron_id=motorNeurons[m], weight= wts[s])
+        for sn in self.SN:
+            firstMN = min(self.MN, key=self.MN.get) #gets first motor neuron
+            sim.send_synapse(source_neuron_id=self.SN[sn], target_neuron_id=self.MN[firstMN], weight=random.random()*2 -1 )
