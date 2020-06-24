30dc6502ea4bc3f2c11efc9fe03bd0f9605c4b41
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-24 22:23:33 +0400

    Update playback.py

--- Final Project/playback.py
+++ Final Project/playback.py
@@ -1,10 +1,11 @@
 from individual import INDIVIDUAL
 import pickle
+import constants as c
 
-f = open('robot.p','rb')
+f = open('0-2K-50best.p','rb')
 best = pickle.load(f)
 f.close()
 
-best.Start_Evaluation(False)
+best.Start_Evaluation(False, c.Max_dist)
 best.Compute_Fitness()
 print(best.fitness)
