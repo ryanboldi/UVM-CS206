a0833a242a4da02e75e88fb879edd25721ba31c9
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-25 09:59:34 +0400

    started small popsize run

--- Final Project/geneticAlgorithm.py
+++ Final Project/geneticAlgorithm.py
@@ -14,7 +14,7 @@ from population import POPULATION
 from multiprocessing import Lock
 
 Load = False
-popSize = 50
+popSize = 5
 
 ## pretrain-total-popsize-eval_time
 saveFile = str(c.gens) + '-' + str(c.totGens) + '-' + str(popSize) + '-' + str(c.eval_time)
