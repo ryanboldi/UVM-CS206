50a7091b2af60566b043559b8111ac1d95700421
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-06-26 23:03:34 +0400

    started adding images of the fitness graph

--- Final Project/avgFitGraph.py
+++ Final Project/avgFitGraph.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt 
+from matplotlib import rc
 import pickle
 import constants as c
 
-experiment = '0-600-100-1500'
+experiment = '300-600-100-1500'
 
 f = open(experiment + '/' + experiment + 'fitness.p','rb')
 fitness = pickle.load(f)
@@ -15,11 +16,17 @@ g.close()
 
 print(fitness)
 
+rc('font',**{'family':'san-serif','sans-serif':['Helvetica']})
+rc('text',usetex=True)
+
 f = plt.figure() #adds a figure
 panel = f.add_subplot(111) #creates drawing panel inside the figure 
 panel.set_xlabel('Evolutionary time')
 #panel.set_ylabel('Average Fitness')
 plt.plot(fitness, label='Average Fitness') #plots data to the panel
 plt.plot(tower, label = 'Tower distance')
+#panel.set_ylim(0,7)
 plt.legend()
+plt.title(experiment)
 plt.show() #shows the figure
+f.savefig(experiment + '/' + experiment + '.png',bbox_inches='tight')
